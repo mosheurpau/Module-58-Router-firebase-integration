d56300fda372319b1fded0fe3119804094bc6564
mosheurpau <mosheur.pau@gamil.com>
2022-04-11 00:06:21 +0600

    Use React Firebase Hook to manage user authentication

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import useFirebase from "../../hooks/useFirebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth, signOut } from "firebase/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
 
 const Header = () => {
-  const { user, handleSingOut } = useFirebase();
+  const [user] = useAuthState(auth);
   return (
     <div className="header">
       <nav>
@@ -14,7 +18,7 @@ const Header = () => {
         <Link to="/register">Register</Link>
         <span>{user?.displayName && user.displayName}</span>
         {user?.uid ? (
-          <button onClick={handleSingOut}>Sign Out</button>
+          <button onClick={() => signOut(auth)}>Sign Out</button>
         ) : (
           <Link to="/login">Login</Link>
         )}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,17 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-import useFirebase from "../../hooks/useFirebase";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
 
 const Login = () => {
-  const { singInWithGoogle } = useFirebase();
+  const [signInWithGoogle, user] = useSignInWithGoogle(auth);
   return (
     <div>
       <h2>Login page</h2>
       <div style={{ margin: "20px" }}>
-        <button onClick={singInWithGoogle}>Google Sing In</button>
+        <button onClick={() => signInWithGoogle()}>Google Sing In</button>
       </div>
       <form>
         <input type="email" placeholder="Your Email" />
