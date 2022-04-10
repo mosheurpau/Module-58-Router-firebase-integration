fd452abd1820feb3781cd3e7b51b919e61d21e6c
mosheurpau <mosheur.pau@gamil.com>
2022-04-10 22:28:30 +0600

    Add Google Sign using custom hook with on State Change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
 
 const Login = () => {
+  const { singInWithGoogle } = useFirebase();
   return (
     <div>
       <h2>Login page</h2>
+      <div style={{ margin: "20px" }}>
+        <button onClick={singInWithGoogle}>Google Sing In</button>
+      </div>
+      <form>
+        <input type="email" placeholder="Your Email" />
+        <br />
+        <input type="password" name="" id="" placeholder="Your Password" />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <h3>Please Register now</h3>
+      <form>
+        <input type="text" placeholder="Your name" /> <br />
+        <input type="email" placeholder="Your Email" />
+        <br />
+        <input type="password" name="" id="" placeholder="Your Password" />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
